39fe639c40c111de2e8cbfc97cb402755bf4f742
beetrootfarmer <pos04118@gmail.com>
2025-11-19 18:18:40 +0900

    feat : 앱바 클릭 시 페이지 이동

--- src/components/hex-appbar.tsx
+++ src/components/hex-appbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import home from "../../public/home.svg"
 import graph from "../../public/graph.svg"
 import rank from "../../public/rank.svg"
@@ -5,13 +7,15 @@ import recommend from "../../public/recommend.svg"
 import Image from "next/image"
 import add from "../../public/add.svg"
 import { FONT_COLORS, FONT_STYLES } from "@/styles/fontStyles"
+import Link from 'next/link';
 
 export const AppBar = () => {
+
     return (
         <div className="w-full h-18 bg-white absolute bottom-0 left-0 flex ">
             <div className="flex px-3 w-[calc((100%-65px)/2)] justify-evenly gap-4">
-                <MenuIcon icon={home} name="홈"/>
-                <MenuIcon icon={graph} name="변화"/>
+                <MenuIcon icon={home} name="홈" path="/hex" />
+                <MenuIcon icon={graph} name="변화" path="/hex/graph" />
             </div>
             <div className="relative w-[65px] h-[65px] rounded-full bg-[#BDB2DD] bottom-5 ">
                 <div className="w-full h-full flex justify-center">
@@ -19,19 +23,22 @@ export const AppBar = () => {
                 </div>
             </div>
             <div className="flex px-3 w-[calc((100%-65px)/2)] justify-evenly gap-4">
-                <MenuIcon icon={rank} name="순위"/>
-            <MenuIcon icon={recommend} name="추천"/>
+                <MenuIcon icon={rank} name="순위" path="/hex/rank"/>
+                <MenuIcon icon={recommend} name="추천" path="/hex/recommend"/>
             </div>
         </div>
     )
 }
 
-const MenuIcon = ({ icon, name }: { icon: any; name: string }) => {
+const MenuIcon = ({ icon, name, path}: { icon: any; name: string; path:string }) => {
     return (
+        <Link href={path}>
+
         <div className="flex flex-col items-center py-2 h-full justify-center items-center gap-1">
             <Image src={icon} alt={name} width={24} height={24}/>
             <p className={FONT_STYLES.body14 + " " + FONT_COLORS.grey}>{name}</p>
         </div>
+        </Link>
     )
 }
     
