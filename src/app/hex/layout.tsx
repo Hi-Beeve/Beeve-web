b11af1299ffc0e184764efac6b6e0d13f31f1b02
beetrootfarmer <pos04118@gmail.com>
2025-11-23 12:08:53 +0900

    feat : 프로필 영역 fixed로 레이아웃분리

--- src/app/hex/layout.tsx
+++ src/app/hex/layout.tsx
@@ -1,10 +1,18 @@
 import { AppBar } from "@/components/hex-appbar";
+import HexProfileHeader from "@/components/hex-profile-header";
 
 export default function HexLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="min-h-screen">
+            <HexProfileHeader user={USER_DATA} />
             <AppBar />
             {children}
         </div>
     );
 }
+
+// TODO : 로그인 시 스토리지에 저장한 데이터를 불러오도록 수정
+const USER_DATA = {
+    name: 'YERIEL',
+    profileImage: 'https://example.com/profile.jpg',
+};
